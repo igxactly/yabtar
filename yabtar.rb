f670370cb7cafc533559a5a0f0e7c27b9161cea6
Ingu Kang <igxactly@gmail.com>
2016-05-16 12:24:13 +0900

    Tried over endianess conversion - 2

--- yabtar.rb
+++ yabtar.rb
@@ -1,22 +1,50 @@
 #!/usr/bin/env ruby
 
 File.open(ARGV[0], "rb") do |f|
+=begin
     puts (data = f.read(64)).unpack('H*')
-
     puts data.unpack('L>*').pack('L<*').unpack('H*')
-
-     #.pack('H*').unpack('N*').pack('V*').unpack('H*')
-=begin
-    (data = f.read(64)).each_byte do |b|
-        puts b.to_s(16)
-    end #.to_s() #.pack('H*').unpack('N*').pack('V*').unpack('H*')
 =end
 
-=begin
-    f.each_byte do |b|
-        printf "%02x ", b
-    end
-=end
+    # __u32 magic;        /* MAGIC << 8 | version */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('H*')
+
+    # __u32 sequence;     /* event number */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('L*')
+
+    # __u64 time;     /* in microseconds */
+    puts (data = f.read(8)).unpack('Q>*').pack('Q<*').unpack('Q*')
+
+    # __u64 sector;       /* disk offset */
+    puts (data = f.read(8)).unpack('Q>*').pack('Q<*').unpack('Q*')
+
+    # __u32 bytes;        /* transfer length */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('L*')
+
+    # __u32 action;       /* what happened */
+    # puts "b%032b" % (data = f.read(4)).unpack('L<') #.pack('L<*')
+    puts "0x%08x" % (data = f.read(4)).unpack('L<') #.pack('L<*')
+
+    # __u32 pid;      /* who did it */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('L*')
+
+    # __u32 device;       /* device number */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('L*')
+
+    # __u32 cpu;      /* on what cpu did it happen */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('L*')
+
+    # __u16 error;        /* completion error */
+    puts (data = f.read(2)).unpack('S>*').pack('S<*').unpack('S*')
+
+    # __u16 pdu_len;      /* length of data after this trace */
+    puts "pdu_len:"
+    puts (data = f.read(2)).unpack('S<')
+    len = data.unpack('S<')[0]
+
+    puts (data = f.read(len))
+
+    #AGAIN
+    # __u32 magic;        /* MAGIC << 8 | version */
+    puts (data = f.read(4)).unpack('L>*').pack('L<*').unpack('H*')
 end
-
-
