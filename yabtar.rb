b726415ba67bad5171d5ab1154eaf5c6082b73e7
Ingu Kang <igxactly@gmail.com>
2016-05-16 12:27:39 +0900

    Renamed variable for each read() call

--- yabtar.rb
+++ yabtar.rb
@@ -2,50 +2,54 @@
 
 def read_and_print_one_record (f)
     # __u32 magic;        /* MAGIC << 8 | version */
-    puts (data = f.read(4)).unpack("H*")
+    puts (d_magic = f.read(4)).unpack("H*")
 
     # __u32 sequence;     /* event number */
-    puts (data = f.read(4)).unpack('L<')
+    puts (d_seq = f.read(4)).unpack('L<')
 
     # __u64 time;     /* in microseconds */
-    puts (data = f.read(8)).unpack('Q<')
+    puts (d_time = f.read(8)).unpack('Q<')
 
     # __u64 sector;       /* disk offset */
-    puts (data = f.read(8)).unpack('Q<')
+    puts (d_sector = f.read(8)).unpack('Q<')
 
     # __u32 bytes;        /* transfer length */
-    puts (data = f.read(4)).unpack('L<')
+    puts (d_bytes = f.read(4)).unpack('L<')
 
     # __u32 action;       /* what happened */
     # puts "b%032b" % (data = f.read(4)).unpack('L<') #.pack('L<*')
-    puts "0x%08x" % (data = f.read(4)).unpack('L<') #.pack('L<*')
+    puts "0x%08x" % (d_action = f.read(4)).unpack('L<') #.pack('L<*')
 
     # __u32 pid;      /* who did it */
-    puts (data = f.read(4)).unpack('L<')
+    puts (d_pid = f.read(4)).unpack('L<')
 
     # __u32 device;       /* device number */
-    puts (data = f.read(4)).unpack('L<')
+    puts (d_dev = f.read(4)).unpack('L<')
 
     # __u32 cpu;      /* on what cpu did it happen */
-    puts (data = f.read(4)).unpack('L<')
+    puts (d_cpu = f.read(4)).unpack('L<')
 
     # __u16 error;        /* completion error */
-    puts (data = f.read(2)).unpack('S<')
+    puts (d_error = f.read(2)).unpack('S<')
 
     # __u16 pdu_len;      /* length of data after this trace */
     puts "pdu_len:"
-    puts (data = f.read(2)).unpack('S<')
-    len = data.unpack('S<')[0]
+    puts (d_len = f.read(2)).unpack('S<')
+    len = d_len.unpack('S<')[0]
 
     if len > 0
-        puts (data = f.read(len))
+        puts (d_extra = f.read(len))
     else
         puts "EXTRA DATA EMPTY"
     end
 end
 
 File.open(ARGV[0], "rb") do |f|
-    while not f.eof
+    #while not f.eof
+    #    read_and_print_one_record(f)
+    #end
+
+    4.times do
         read_and_print_one_record(f)
     end
 end
