9bf7b5ac126f3167ae964924635be3165f8b32e9
Ingu Kang <igxactly@gmail.com>
2016-05-16 12:23:17 +0900

    Tried over endianess conversion

--- yabtar.rb
+++ yabtar.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 
-File.open(ARGV[0]) do |f|
+File.open(ARGV[0], "rb") do |f|
+    puts (data = f.read(64)).unpack('H*')
+
+    puts data.unpack('L>*').pack('L<*').unpack('H*')
+
+     #.pack('H*').unpack('N*').pack('V*').unpack('H*')
+=begin
+    (data = f.read(64)).each_byte do |b|
+        puts b.to_s(16)
+    end #.to_s() #.pack('H*').unpack('N*').pack('V*').unpack('H*')
+=end
+
+=begin
     f.each_byte do |b|
-        printf "0x%02x\n", b #.to_s(16)
+        printf "%02x ", b
     end
+=end
 end
 
 
